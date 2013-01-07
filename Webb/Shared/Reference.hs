0a4dd06bb9c0f4c0df73b5578245ab31a2d00d7c
Joe Fredette <jfredett@gmail.com>
2013-01-07 15:29:15 -0500

    flesh out Reference model

--- Webb/Shared/Reference.hs
+++ Webb/Shared/Reference.hs
@@ -1,1 +1,13 @@
-module Webb.Shared.Reference where
+module Webb.Shared.Reference (
+  refer, Reference
+) where
+
+newtype Reference = Ref String
+  deriving (Eq, Ord)
+
+instance Show Reference where
+  show (Ref s) = "[" ++ s ++ "]"
+
+refer :: String -> Reference
+refer = Ref
+
